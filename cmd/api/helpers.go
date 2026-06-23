c9d351f1249a9ac774b788c0fa47650028ac8c4c
ukiran03 <ushakiranreddi@gmail.com>
2026-06-23 22:00:45 +0530

    feat(api): add readJSON helper to read request, and temp data

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
 	"net/http"
+	"strings"
 )
 
 type envelope map[string]any
@@ -25,6 +29,65 @@ func (app *application) writeJSON(
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	w.Write(js)
+
+	return nil
+}
+
+func (app *application) readJSON(
+	w http.ResponseWriter, r *http.Request, dst any,
+) error {
+	r.Body = http.MaxBytesReader(w, r.Body, 1_048_576)
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	err := dec.Decode(dst)
+	if err != nil {
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+		var invalidUnmarshalError *json.InvalidUnmarshalError
+		var maxBytesError *http.MaxBytesError
+
+		switch {
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf(
+				"body contains badly-formed JSON (at character %d)",
+				syntaxError.Offset)
+
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains badly-formed JSON")
+
+		case errors.As(err, &unmarshalTypeError):
+			if unmarshalTypeError.Field != "" {
+				return fmt.Errorf(
+					"body contains incorrect JSON type for field %q",
+					unmarshalTypeError.Field)
+			}
+			return fmt.Errorf(
+				"body contains incorrect JSON type (at character %d)",
+				unmarshalTypeError.Offset)
+
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field")
+			return fmt.Errorf("body contains unknown key %s", fieldName)
+
+		case errors.As(err, &maxBytesError):
+			return fmt.Errorf(
+				"body must not be larger than %d bytes", maxBytesError.Limit)
+
+		case errors.As(err, &invalidUnmarshalError):
+			panic(err)
+
+		default:
+			return err
+		}
+	}
+	err = dec.Decode(&struct{}{})
+	if !errors.Is(err, io.EOF) {
+		return errors.New("body must only contain a single JSON value")
+	}
 
 	return nil
 }
--- cmd/api/minurls.go
+++ cmd/api/minurls.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"ukiran.com/minurl/internal/data"
@@ -10,8 +11,22 @@ import (
 
 // POST /v1/shorten
 func (app *application) createMinurlHandler(w http.ResponseWriter, r *http.Request) {
-	slug := chi.URLParam(r, "slug")
-	fmt.Fprintf(w, "create a new minurl for %s", slug)
+	var input struct {
+		Slug      string     `json:"slug"`
+		URL       string     `json:"url"`
+		Title     *string    `json:"title,omitzero"`
+		IsCustom  bool       `json:"is_custom"`
+		ExpiresAt *time.Time `json:"expires_at"`
+		UserID    *int64     `json:"user_id,omitzero"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 // GET /{slug}
@@ -34,12 +49,25 @@ func (app *application) redirectHandler(w http.ResponseWriter, r *http.Request) 
 // GET /v1/minurls/{slug}
 func (app *application) getMinurlHandler(w http.ResponseWriter, r *http.Request) {
 	slug := chi.URLParam(r, "slug")
-	fmt.Fprintf(w, "get the URL for %s", slug)
+
+	minurl := data.MinUrl{
+		Slug:      slug,
+		URL:       "https://example.com",
+		Title:     new("Example"),
+		IsCustom:  false,
+		CreatedAt: time.Now(),
+		ExpiresAt: new(time.Now().AddDate(0, 0, 3)),
+		UserID:    new(int64(1)),
+	}
+
+	err := app.writeJSON(w, http.StatusOK, envelope{"minurl": minurl}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
 
 // DELETE /v1/minurls/{slug}
 func (app *application) deleteMinurlHandler(w http.ResponseWriter, r *http.Request) {
 	slug := chi.URLParam(r, "slug")
 	fmt.Fprintf(w, "delete the minURL %s", slug)
-	return
 }
