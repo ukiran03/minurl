dbd52cb512ce8c92935bc691ddaded1b96dd122d
ukiran03 <ushakiranreddi@gmail.com>
2026-06-23 11:37:15 +0530

    refactor(api): modify recoverPanic middleware
    closer to middleware.Recoverer

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -3,17 +3,28 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"runtime/debug"
 )
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
-			pv := recover()
-			if pv != nil {
+			if rvr := recover(); rvr != nil {
+				if rvr == http.ErrAbortHandler {
+					panic(rvr)
+				}
+
+				app.logger.Error(
+					"panic recovered",
+					"panic", rvr,
+					"stack", string(debug.Stack()),
+				)
+
 				w.Header().Set("Connection", "close")
-				app.serverErrorResponse(w, r, fmt.Errorf("%v", pv))
+				app.serverErrorResponse(w, r, fmt.Errorf("%v", rvr))
 			}
 		}()
+
 		next.ServeHTTP(w, r)
 	})
 }
