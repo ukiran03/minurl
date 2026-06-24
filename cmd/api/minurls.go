b2f61e98f4ab4a4b14156a07bbd1f2b1e900cbec
ukiran03 <ushakiranreddi@gmail.com>
2026-06-24 11:03:17 +0530

    refactor(api): decode directly into MinUrl in create handler
    - Remove redundant internal input struct from createMinurlHandler.
    - Decode JSON directly into data.MinUrl domain model.
    - Leverage Lifespan's custom UnmarshalJSON for automatic time parsing and
    validation.

--- cmd/api/minurls.go
+++ cmd/api/minurls.go
@@ -7,43 +7,63 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"ukiran.com/minurl/internal/data"
+	"ukiran.com/minurl/internal/validator"
 )
 
 // POST /v1/shorten
 func (app *application) createMinurlHandler(w http.ResponseWriter, r *http.Request) {
-	var input struct {
-		Slug      string     `json:"slug"`
-		URL       string     `json:"url"`
-		Title     *string    `json:"title,omitzero"`
-		IsCustom  bool       `json:"is_custom"`
-		ExpiresAt *time.Time `json:"expires_at"`
-		UserID    *int64     `json:"user_id,omitzero"`
-	}
+	var minurl data.MinUrl
 
-	err := app.readJSON(w, r, &input)
+	err := app.readJSON(w, r, &minurl)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
 		return
 	}
 
-	fmt.Fprintf(w, "%+v\n", input)
+	// At this exact point, minurl.Created is set to time.Now()
+	// and minurl.Expiry has been calculated and jittered!
+	// see data.Minurl.UnmarshalJSON()
+
+	v := validator.New()
+
+	// URL validation
+	v.Check(minurl.URL != "", "url", "must be provided")
+	v.Check(
+		len(minurl.URL) >= 11 && len(minurl.URL) <= 2048,
+		"url", "must be between 11 and 2048 characters long",
+	)
+
+	// Slug validation
+	if minurl.IsCustom {
+		v.Check(len(minurl.Slug) >= 8 && len(minurl.Slug) <= 100,
+			"custom slug", "must be between 8 and 100 characters long")
+		v.Check(!validator.Matches(minurl.Slug, validator.SlugRegex),
+			"custom slug", ErrInvalidCustomSlug.Error())
+	}
+
+	// Title validation (Using pointer safe-check)
+	if minurl.Title != nil && *minurl.Title != "" {
+		v.Check(len(*minurl.Title) <= 100,
+			"title", "must not be more than 100 characters long")
+	}
+
+	// UserID validation
+	if minurl.UserID != nil {
+		v.Check(*minurl.UserID > 0, "user_id", "must be a valid positive integer")
+	}
+
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+	// Now you can pass `minurl` directly to your database insert function!
+	fmt.Fprintf(w, "%+v\n", minurl)
 }
 
 // GET /{slug}
 func (app *application) redirectHandler(w http.ResponseWriter, r *http.Request) {
 	slug := chi.URLParam(r, "slug")
-
-	longURL, exists := data.UrlDatabase[slug]
-	if !exists {
-		http.Error(w, "Short URL not found", http.StatusNotFound)
-		return
-	}
-
-	// Optional: Track analytics here asynchronously
-	// go trackClick(shortCode, r)
-
-	// Redirect the user using a 302 Found status
-	http.Redirect(w, r, longURL, http.StatusFound)
+	fmt.Fprintf(w, "%s", slug)
 }
 
 // GET /v1/minurls/{slug}
@@ -51,13 +71,16 @@ func (app *application) getMinurlHandler(w http.ResponseWriter, r *http.Request)
 	slug := chi.URLParam(r, "slug")
 
 	minurl := data.MinUrl{
-		Slug:      slug,
-		URL:       "https://example.com",
-		Title:     new("Example"),
-		IsCustom:  false,
-		CreatedAt: time.Now(),
-		ExpiresAt: new(time.Now().AddDate(0, 0, 3)),
-		UserID:    new(int64(1)),
+		Slug:     slug,
+		URL:      "https://example.com",
+		Title:    new("Example"),
+		IsCustom: false,
+
+		UserID: new(int64(1)),
+		Lifespan: data.Lifespan{
+			Created: time.Now(),
+			Expiry:  time.Now().AddDate(0, 0, 3),
+		},
 	}
 
 	err := app.writeJSON(w, http.StatusOK, envelope{"minurl": minurl}, nil)
